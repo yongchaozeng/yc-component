dba32f0fdf94ea0d3568bd0cb72e659a57d4724e
zyc <491476277@qq.com>
2020-02-03 22:32:54 +0800

    input受控组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// import Button from './components/button'
 import Button from './components/button'
+import Input from './components/input'
 
 
 const App: React.FC = () => {
-  const submit = (e:React.SyntheticEvent)=>{
+
+  let [value, setValue] = useState('')
+
+  const submit = (e: React.SyntheticEvent) => {
     console.log('submit')
+  }
+  const onChange = (e: string) => {
+    // setValue((e.target as HTMLTextAreaElement).value)
+    setValue(e)
   }
   return (
     <div className="App">
-      <Button onClick={(e:React.SyntheticEvent)=>{submit(e)}} >23</Button>
+      <Button onClick={(e: React.SyntheticEvent) => { submit(e) }} >23</Button>
+      <Input value={value} onChange={onChange} />
+      <h1>{value}</h1>
     </div>
   );
 }
--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,14 +1,47 @@
-import React, {FC} from 'react'
+import React, { FC ,useState} from 'react'
 import './index.less'
-var classNames = require('classnames');
+const classNames = require('classnames');
+
 
 interface Input {
-
+    placeholder?: string,
+    close?: boolean,
+    value?:string,
+    onChange:(e:string)  => void 
 }
 
-const Input: FC<Input> = (props) => {
+const Input: FC<Input> = ({ placeholder = '请输入', close = true,value='',onChange }) => {
+
+    let [closeShow,setCloseShow] = useState(false)
+
+    // const setValue =(value:string)=>{
+    //     onChange(value);
+    // } 
+    const change = (e:React.ChangeEvent<HTMLInputElement|HTMLTextAreaElement>)=>{
+        onChange(e.target.value);
+        // setValue(e.target.value)
+        // onChange((e.target as HTMLTextAreaElement).value);
+    //     if((e.target as HTMLTextAreaElement).value.length>0){ 
+    //         setCloseShow(true)
+    //     }else{
+    //         setCloseShow(false)
+    //     }
+         
+    }
+
+    const clear = ()=>{
+        onChange('');
+        // setValue('')
+        // console.log(11);
+        // (e.target as HTMLTextAreaElement).value = ''
+    }
+
     return (
-       <h1>111</h1>
+        <span className='input-container'>
+            <input value={value}  type="text" onChange={change} placeholder={placeholder} />
+            <i onClick={clear} className={classNames({'input-close':close && value.length>0},'input-close-hidden')}>x</i>
+        </span>
+
     )
 
 }
