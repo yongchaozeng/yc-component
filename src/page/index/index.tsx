feab1afa6f962fc0311797210a4d122dd985036a
zengyongchao <l>
2020-02-21 17:54:32 +0800

    开始写业务

---
+++ src/page/index/index.tsx
@@ -0,0 +1,16 @@
+import React, {FC} from 'react'
+import './index.less'
+var classNames = require('classnames');
+
+interface Home {
+
+}
+
+const Home: FC<Home> = (props) => {
+    return (
+        <div>123</div>
+    )
+
+}
+
+export default Home
